d7869503bff588bc96d5e309974fcea1ff5a4916
Kella Bulghur <kellabul@student.21-school.ru>
2022-08-24 16:23:24 +0300

    input validation in progress

---
+++ src/s21_smartcalc.h
@@ -0,0 +1,12 @@
+#ifndef SRC_S21_SMARTCALC_H_
+#define SRC_S21_SMARTCALC_H_
+
+int input_validation(char *string);
+
+
+
+// santa's little helpers
+int isNumber(char element);
+int isOperation(char element);
+
+#endif  // SRC_S21_SMARTCALC_H_
---
+++ src/s21_smartcalc_parcing.c
@@ -0,0 +1,33 @@
+#include "s21_smartcalc.h"
+
+int input_validation(char *string) {
+  int result = 0;
+  int braces = 0;
+  for (int i = 0; string[i]; i++) {
+    if (isOperation(string[i]) && !isNumber(string[i + 1])) {
+      result = -1;
+      break;
+    } else if (string[i] == '(') {
+      braces++;
+    } else if (string[i] == ')') {
+      braces--;
+    } else if (!isNumber(string[i])) {
+      result = -1;
+      break;
+    }
+  }
+  if (braces) result = -1;
+  return result;
+}
+
+int isOperation(char element) {
+  int result = 0;
+  if (element == '+' || element == '-' || element == '*' || element == '/') result = 1;
+  return result;
+}
+
+int isNumber(char element) {
+  int result = 0;
+  if (element >= '0' && element <= '9') result = 1;
+  return result;
+}
