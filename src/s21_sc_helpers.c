a07e88307e3881286c699a34dc7afd6b494ff6cb
Kella Bulghur <kellabul@student.21-school.ru>
2022-09-13 10:07:08 +0300

    tokensToString file

---
+++ src/s21_sc_helpers.c
@@ -0,0 +1,15 @@
+#include "s21_smartcalc.h"
+#include <string.h>
+#include <stdio.h>
+
+void convertTokensToString(s_tokens *tokens, char *string) {
+  for (int i = 0; tokens[i].type; i++) {
+    char buffer_string[100] = {};
+    if (tokens[i].type == 1) {
+      sprintf(buffer_string, "%g", tokens[i].value);
+    } else {
+      sprintf(buffer_string, "%c", (char)tokens[i].value);
+    }
+    strcat(string, buffer_string);
+  }
+}
