0e38f6c96a704d7b3260efa62c1d5cee40d85515
Kella Bulghur <kellabul@student.21-school.ru>
2022-09-25 23:24:36 +0300

    gtk entry logic was added

--- src/s21_gtk_sc_main.c
+++ src/s21_gtk_sc_main.c
@@ -5,6 +5,7 @@
 
 
 GtkEntry *entry_exp;
+GtkWidget *main_label_error;
 char input[S21_MAX_INPUT] = {};
 int input_index = 0;
 
@@ -17,8 +18,9 @@ int main(int argc, char *argv[]) {
   builder = gtk_builder_new_from_file("GUI/s21_smartcal_gui.glade");
 
   window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+  main_label_error = GTK_WIDGET(gtk_builder_get_object(builder, "main_label_error"));
   entry_exp = GTK_ENTRY(gtk_builder_get_object(builder, "entry_exp"));
-
+  
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
   gtk_builder_connect_signals(builder, NULL);
@@ -30,6 +32,8 @@ int main(int argc, char *argv[]) {
   return EXIT_SUCCESS;
 }
 
+/* ============= BUTTONS ============= */ 
+
 void button_1_clicked_cb() {
   input_index++;
   strcat(input, "1");
@@ -38,7 +42,7 @@ void button_1_clicked_cb() {
 
 void button_delete_clicked_cb() {
   input_index++;
-  strcat(input, "1");
+  input[strlen(input) - 1] = '\0';
   gtk_entry_set_text(entry_exp, (const gchar *)input);
 }
 
@@ -49,7 +53,15 @@ void button_log_clicked_cb() {
 }
 
 
+/* ============= ENTRY ============= */ 
+
 
 void entry_exp_changed_cb(GtkEntry *entry){
-  sprintf(input, "%s", gtk_entry_get_text(entry));
+  const char *buffer = gtk_entry_get_text(entry);
+  if (strlen(buffer) > S21_MAX_INPUT) {
+    gtk_label_set_text (GTK_LABEL(main_label_error), (const gchar* ) "TOO MUCH ARGUMENTS");
+  } else {
+    sprintf(input, "%s", buffer);
+    gtk_label_set_text (GTK_LABEL(main_label_error), (const gchar* ) "");
+  }
 }
